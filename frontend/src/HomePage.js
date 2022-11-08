20c06254e3f245daeb420f36418cbf6af4e42a43
IT20005580 <88153854+Thimira99@users.noreply.github.com>
2022-11-09 01:40:12 +0530

    add login and registration frontend

---
+++ frontend/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+const HomePage = () => {
+	return (
+		<View>
+			<Text>Welcome</Text>
+		</View>
+	);
+};
+
+export default HomePage;
