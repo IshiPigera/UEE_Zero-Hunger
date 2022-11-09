0e719d2f1bdae74a53206f2b1fbdc56eb07b5807
suki119 <it20045326@my.sliit.lk>
2022-11-09 17:56:37 +0530

    added User mainpage

--- frontend/FrontUser/NormalUser/HomeMainPage.js
+++ frontend/FrontUser/NormalUser/HomeMainPage.js
@@ -4,21 +4,44 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // Screens
-import NewsFeed from './NewsFeed';
+import NewsFeedPage from './NewsFeed';
 
+//Screen names
+const NewsFeed = 'NewsFeed';
 
+const Tab = createBottomTabNavigator();
 
 export default class HomeMainPage extends React.Component {
 
 
     render(){
         return(
-            <View>
+            <Tab.Navigator
+			initialRouteName={NewsFeed}
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+					let rn = route.name;
 
-                <Text>
-                    hi sukitha
-                </Text>
-            </View>
+					if (rn === NewsFeed) {
+						iconName = focused ? 'NewsFeed' : 'home-outline';
+					} 
+
+					// You can return any component that you like here!
+					return <Ionicons name={iconName} size={size} color={color} />;
+				},
+			})}
+			tabBarOptions={{
+				activeTintColor: 'tomato',
+				inactiveTintColor: 'grey',
+				labelStyle: { paddingBottom: 10, fontSize: 10 },
+				style: { padding: 10, height: 70 },
+			}}
+		>
+			<Tab.Screen name={homeName} component={NewsFeedPage} />
+			{/* <Tab.Screen name={detailsName} component={DetailsScreen} />
+			<Tab.Screen name={settingsName} component={SettingsScreen} /> */}
+		</Tab.Navigator>
         )
     }
 
